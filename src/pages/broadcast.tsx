e31ce77fad4aab5c4371f457d5ece8364605b201
mu0363 <dde036@gmail.com>
2021-10-18 22:17:21 +0900

    サインイン画面とヘッダーUIを実装

---
+++ src/pages/broadcast.tsx
@@ -0,0 +1,17 @@
+const Broadcast = () => {
+  return (
+    <div className="h-screen bg-gray-100">
+      <div className="flex items-center py-5 px-4 bg-white shadow-sm">
+        <div className="flex flex-grow items-center">
+          <img className="h-10" src="/engivia_logo.png" alt="logo" />
+          <h1 className="ml-4 text-xl font-bold text-[#0284C7]">
+            エンジビアの泉
+          </h1>
+        </div>
+        <img className="h-10 rounded-full" src="/15007672.jpeg" alt="avatar" />
+      </div>
+    </div>
+  );
+};
+
+export default Broadcast;
