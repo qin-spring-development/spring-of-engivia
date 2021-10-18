9d452a42aab9ed6d5955b68a76b2818bd92f1f1d
shogo4131 <shougo80697118@gmail.com>
2021-10-19 01:25:00 +0900

    feat: add storybook

---
+++ src/components/Button/index.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+import { Button, Props } from "./index";
+
+export default {
+  component: Button,
+  title: "Components/Button",
+} as Meta;
+
+const Template: Story<Props> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  type: "button",
+  children: "登録",
+  isSubmitting: false,
+};
