2f4e443a64cd8be5c95faa757d2405f0bfa6890a
mu0363 <dde036@gmail.com>
2021-10-23 19:21:25 +0900

    tailwindのclassname順番を修正

--- src/pages/feature.tsx
+++ src/pages/feature.tsx
@@ -57,22 +57,22 @@ const Feature = () => {
   ];
 
   return (
-    <div className="h-screen bg-gray-100 container mx-auto">
+    <div className="container mx-auto h-screen bg-gray-100">
       <div className="flex justify-between items-center pt-20">
-        <div className="text-center mx-auto pl-80">
+        <div className="pl-80 mx-auto text-center">
           {/* <p className='text-[#065F46] bg-[#D1FAE5] rounded-2xl inline px-3 py-1'>放送中</p> */}
-          <p className="text-[#9A3412] bg-[#FFEDD5] rounded-2xl inline px-3 py-1">
+          <p className="inline py-1 px-3 text-[#9A3412] bg-[#FFEDD5] rounded-2xl">
             放送前・エンジビア募集中
           </p>
-          <h1 className="text-[#111827] font-bold text-3xl mt-4">
+          <h1 className="mt-4 text-3xl font-bold text-[#111827]">
             第４回エンジビアの泉
           </h1>
         </div>
         <div>
-          <button className="bg-[#0284C7] text-white px-8 py-3 rounded-md">
-            放送を開始する
+          <button className="py-3 px-8 text-white bg-[#0284C7] rounded-md">
+            放送を開始するよ
           </button>
-          <button className="px-8 py-3 rounded-md bg-[#E0F2FE] text-[#0369A1] ml-8">
+          <button className="py-3 px-8 ml-8 text-[#0369A1] bg-[#E0F2FE] rounded-md">
             編集する
           </button>
         </div>
@@ -82,7 +82,7 @@ const Feature = () => {
           {FeatureText?.map((text) => (
             <p
               key={text}
-              className="text-center w-96 h-14 pt-4 bg-gray-300 text-[#111827] rounded-lg"
+              className="pt-4 w-96 h-14 text-center text-[#111827] bg-gray-300 rounded-lg"
             >
               {text}
             </p>
@@ -92,61 +92,61 @@ const Feature = () => {
           <div>
             {beforeFeatureData.map((data) => (
               <div
-                className="bg-white w-96 h-24 rounded-lg p-4 mt-4"
+                className="p-4 mt-4 w-96 h-24 bg-white rounded-lg"
                 key={data.id}
               >
                 <p className="text-sm text-[#111827]">{data.text}</p>
                 <div className="flex items-center pt-4">
                   <img
-                    className="h-6 w-6 rounded-full"
+                    className="w-6 h-6 rounded-full"
                     src={data.image}
                     alt="avatar"
                   />
-                  <p className="text-xs text-[#374151] pl-4">{data.name}</p>
+                  <p className="pl-4 text-xs text-[#374151]">{data.name}</p>
                 </div>
               </div>
             ))}
           </div>
           <div>
-            <div className="w-96 h-24 rounded-lg mt-4 border-dashed border-2 border-gray-200">
-              <p className="text-center pt-9 text-[#9CA3AF]">
+            <div className="mt-4 w-96 h-24 rounded-lg border-2 border-gray-200 border-dashed">
+              <p className="pt-9 text-center text-[#9CA3AF]">
                 フィーチャーする
               </p>
             </div>
             {featuringData?.map((data) => (
               <div
-                className="bg-white w-96 h-24 rounded-lg p-4 mt-4"
+                className="p-4 mt-4 w-96 h-24 bg-white rounded-lg"
                 key={data.id}
               >
                 <p className="text-sm text-[#111827]">{data.text}</p>
                 <div className="flex items-center pt-4">
                   <img
-                    className="h-6 w-6 rounded-full"
+                    className="w-6 h-6 rounded-full"
                     src={data.image}
                     alt="avatar"
                   />
-                  <p className="text-xs text-[#374151] pl-4">{data.name}</p>
+                  <p className="pl-4 text-xs text-[#374151]">{data.name}</p>
                 </div>
               </div>
             ))}
-            <button className="bg-[#0284C7] text-white px-6 py-3 rounded-md block mx-auto mt-4">
+            <button className="block py-3 px-6 mx-auto mt-4 text-white bg-[#0284C7] rounded-md">
               タイトルコールする
             </button>
           </div>
           <div>
             {afterFeatureData?.map((data) => (
               <div
-                className="bg-white w-96 h-24 rounded-lg p-4 mt-4"
+                className="p-4 mt-4 w-96 h-24 bg-white rounded-lg"
                 key={data.id}
               >
                 <p className="text-sm text-[#111827]">{data.text}</p>
                 <div className="flex items-center pt-4">
                   <img
-                    className="h-6 w-6 rounded-full"
+                    className="w-6 h-6 rounded-full"
                     src={data.image}
                     alt="avatar"
                   />
-                  <p className="text-xs text-[#374151] pl-4">{data.name}</p>
+                  <p className="pl-4 text-xs text-[#374151]">{data.name}</p>
                 </div>
               </div>
             ))}
