54718c2d8349c5969274d3c85a961dc103179d81
compact_pro git config --global user.name compact_pro <gppmc689@yahoo.co.jp>
2021-11-06 21:43:12 +0900

    放送一覧更新

--- src/pages/broadcast.tsx
+++ src/pages/broadcast.tsx
@@ -3,7 +3,6 @@ import { BroadcastList } from "src/components/BroadcastList";
 const Broadcast = () => {
   return (
     <BaseLayout title="放送一覧">
-      <p>ここに画面を書く</p>
       <BroadcastList />
     </BaseLayout>
   );
