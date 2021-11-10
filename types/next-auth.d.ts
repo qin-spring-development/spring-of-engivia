3c2bd53cbb75324cea31253bce41e4618c4501c6
magako <qutianh@gmail.com>
2021-11-10 22:29:35 +0900

    feat:nextAuthのSessionとUser型の定義を追加

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,12 +1,15 @@
 import NextAuth from "next-auth";
 
 declare module "next-auth" {
+  interface User {
+    id: string;
+    email: string;
+    isAdmin: boolean;
+    name: string;
+    provider: string;
+    image: string;
+  }
   interface Session {
-    user: {
-      email: string;
-      name: string;
-      image: string;
-      id: string;
-    };
+    user: User;
   }
 }
