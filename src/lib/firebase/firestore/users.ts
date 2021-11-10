ec91109887b819955423d0c08fabfc3b0d7db049
magako <qutianh@gmail.com>
2021-11-10 22:28:44 +0900

    fix:firebaseへのリクエスト・レスポンス時のuser型の定義追加

--- src/lib/firebase/firestore/users.ts
+++ src/lib/firebase/firestore/users.ts
@@ -1,19 +1,27 @@
 import { db } from "src/lib/firebase/config";
 
-export type FormatUser = {
+export type ReqUser = {
   uid: string;
-  email: string | null;
-  name: string | null;
-  photoURL: string | undefined;
-  provider: string | undefined;
+  email: string;
+  name: string;
+  image: string;
+  provider: string;
+};
+
+export type ResUser = {
+  uid: string;
+  email: string;
+  name: string;
+  isAdmin: boolean;
+  image: string;
+  provider: string;
 };
 
 const adminUsers = [""];
 
-export const createUser = (user: FormatUser) => {
+export const createUser = (user: ReqUser) => {
   const isAdmin = adminUsers.includes(user.uid);
-  return db
-    .collection("users")
+  db.collection("users")
     .doc(user.uid)
     .set({ ...user, isAdmin }, { merge: true });
 };
