6dbf74cb70021109341e22d0dbd1e608a1fc9181
mu0363 <dde036@gmail.com>
2021-11-11 11:58:06 +0900

    放送中・放送後ページを実装

---
+++ src/pages/broadcast-after.tsx
@@ -0,0 +1,35 @@
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { BaseLayout } from "src/components/Layouts/BaseLayout";
+import { BroadcastTitle } from "src/components/Broadcast/BroadcastTitle";
+import { useSubscribeEngivias } from "src/hooks/useSubscribe";
+import { EngiviaList } from "src/components/Engivia/EngiviaList";
+import { deleteBroadcast } from "src/lib/db";
+
+const BroadcastAfter: NextPage = () => {
+  const router = useRouter();
+  const broadcastId = router.query.id as string;
+  const engivias = useSubscribeEngivias(broadcastId);
+
+  const onDeleteBroadcast = () => {
+    deleteBroadcast(broadcastId);
+    router.push("/broadcasts");
+  };
+
+  return (
+    <BaseLayout title="放送前">
+      <div className="flex relative justify-center items-center">
+        <BroadcastTitle />
+        <button
+          onClick={onDeleteBroadcast}
+          className="absolute right-0 py-2 px-4 mr-2 text-blue-600 bg-light-blue-100 rounded-md"
+        >
+          削除する
+        </button>
+      </div>
+      {engivias && <EngiviaList engivias={engivias} />}
+    </BaseLayout>
+  );
+};
+
+export default BroadcastAfter;
