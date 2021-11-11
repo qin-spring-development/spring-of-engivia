c6d6c6cf7906ca96e34b1a34d4f3c0409a2a6836
mu0363 <dde036@gmail.com>
2021-11-11 11:44:58 +0900

    SWRConfigを削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,16 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { Provider } from "next-auth/client";
 import { SWRConfig } from "swr";
-import { fetcher } from "src/utils/fetcher";
+// import { fetcher } from "src/utils/fetcher";
 import { Auth } from "src/lib/auth/AuthCheck";
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <Provider session={pageProps.session}>
       <Auth pathname={router.pathname}>
-        <SWRConfig value={{ fetcher }}>
-          <Component {...pageProps} />
-        </SWRConfig>
+        {/* <SWRConfig value={{ fetcher }}> */}
+        <Component {...pageProps} />
+        {/* </SWRConfig> */}
       </Auth>
     </Provider>
   );
