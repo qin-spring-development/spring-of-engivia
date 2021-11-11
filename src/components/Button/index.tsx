4fa5d0878fcc26d0a8456673b7dda2147013491c
mu0363 <dde036@gmail.com>
2021-11-11 13:13:42 +0900

    共通Buttonのスタイルを変更

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,34 @@
-import { FC, ReactNode } from "react";
+import type { FC, ReactNode, MouseEventHandler } from "react";
 
 export interface Props {
   isSubmitting: boolean;
   type: "button" | "submit" | "reset";
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  primary: boolean;
   children?: ReactNode;
 }
 
-export const Button: FC<Props> = ({ isSubmitting, type, children }) => {
+export const Button: FC<Props> = ({
+  isSubmitting,
+  type,
+  onClick,
+  primary,
+  children,
+}) => {
   return (
     <>
-      <button disabled={isSubmitting} type={type} className="bg-blue-400">
+      <button
+        disabled={isSubmitting}
+        type={type}
+        onClick={onClick}
+        className={`py-2 px-4 mr-2 rounded-md
+          ${
+            primary
+              ? "bg-light-blue-600 text-white"
+              : "bg-light-blue-100 text-light-blue-700"
+          } 
+        `}
+      >
         {children}
       </button>
     </>
