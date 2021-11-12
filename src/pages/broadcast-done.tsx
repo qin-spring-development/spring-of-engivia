980e416d42c80ed1e332649bc52451273ed3f806
mu0363 <dde036@gmail.com>
2021-11-12 16:10:21 +0900

    ページ構成を変更

--- src/pages/broadcast-done.tsx
+++ src/pages/broadcast-done.tsx
@@ -3,13 +3,15 @@ import { useRouter } from "next/router";
 import { BaseLayout } from "src/components/Layouts/BaseLayout";
 import { BroadcastTitle } from "src/components/Broadcast/BroadcastTitle";
 import { useSubscribeEngivias } from "src/hooks/useSubscribe";
+import { useIsAdmin } from "src/hooks/useSharedState";
 import { EngiviaList } from "src/components/Engivia/EngiviaList";
 import { deleteBroadcast } from "src/lib/db";
 
-const BroadcastAfter: NextPage = () => {
+const BroadcastDone: NextPage = () => {
   const router = useRouter();
   const broadcastId = router.query.id as string;
   const engivias = useSubscribeEngivias(broadcastId);
+  const { isAdmin } = useIsAdmin();
 
   const onDeleteBroadcast = () => {
     deleteBroadcast(broadcastId);
@@ -19,17 +21,19 @@ const BroadcastAfter: NextPage = () => {
   return (
     <BaseLayout title="放送前">
       <div className="flex relative justify-center items-center">
-        <BroadcastTitle />
-        <button
-          onClick={onDeleteBroadcast}
-          className="absolute right-0 py-2 px-4 mr-2 text-blue-600 bg-light-blue-100 rounded-md"
-        >
-          削除する
-        </button>
+        <BroadcastTitle broadcastId={broadcastId} />
+        {isAdmin && (
+          <button
+            onClick={onDeleteBroadcast}
+            className="absolute right-0 py-2 px-4 mr-2 text-blue-600 bg-light-blue-100 rounded-md"
+          >
+            削除する
+          </button>
+        )}
       </div>
       {engivias && <EngiviaList engivias={engivias} />}
     </BaseLayout>
   );
 };
 
-export default BroadcastAfter;
+export default BroadcastDone;
