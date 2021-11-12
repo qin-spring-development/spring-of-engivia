319b130c959068d46f88a20d37c3a956fe21d65e
boo <standingup.boo@gmail.com>
2021-11-12 22:10:57 +0900

    ♻️ 三項演算子からJSに変更

--- src/components/Label/index.tsx
+++ src/components/Label/index.tsx
@@ -1,27 +1,41 @@
 import { FC } from "react";
+//classNameの文字列を配列として読み込んで、スペースをつけて返す関数
+const classNamesSet = (...classes: string[]) => {
+  return classes.filter(Boolean).join(" ");
+};
 
 export type Props = {
   className?: string;
-  status: string;
+  status: "BEFORE" | "IN_PROGRESS" | "DONE";
 };
 
 export const Label: FC<Props> = ({ className, status }) => {
+  let broadcastStatus = "";
+  let textColor = "";
+
+  switch (status) {
+    case "BEFORE":
+      broadcastStatus = "放送前・エンジビア募集中";
+      textColor = "text-orange-700 bg-orange-100";
+      break;
+    case "IN_PROGRESS":
+      broadcastStatus = "放送中";
+      textColor = "text-green-700 bg-green-100";
+      break;
+    case "DONE":
+      broadcastStatus = "放送済";
+      textColor = "text-gray-900 bg-gray-200";
+      break;
+    default:
+      break;
+  }
   return (
     <div className={className}>
       <span
-        className={`py-1 px-3 text-sm rounded-full ${
-          status === "BEFORE"
-            ? "text-orange-700 bg-orange-100"
-            : status === "IN_PROGRESS"
-            ? "text-green-700 bg-green-100"
-            : "text-gray-900 bg-gray-200"
-        }`}
+        className={classNamesSet(textColor, "py-1 px-3 text-sm rounded-full")}
       >
-        {status === "BEFORE"
-          ? "放送前・エンジビア募集中"
-          : status === "IN_PROGRESS"
-          ? "放送中"
-          : "放送済"}
+        {/* <span className={"py-1 px-3 text-sm rounded-full ${textColor}"}> */}
+        {broadcastStatus}
       </span>
     </div>
   );
