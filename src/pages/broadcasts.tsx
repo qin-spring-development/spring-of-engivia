f29e170e2eb3b7bd95f1d6a9a266df1f07b166eb
mu0363 <dde036@gmail.com>
2021-11-12 16:29:41 +0900

    コメントアウトを解除

--- src/pages/broadcasts.tsx
+++ src/pages/broadcasts.tsx
@@ -4,21 +4,21 @@ import { BaseLayout } from "src/components/Layouts/BaseLayout";
 import { BroadCastList } from "src/components/Broadcast/BroadcastList";
 import { BroadcastBefore } from "src/components/Broadcast/BroadcastBefore";
 import { useBroadcastId } from "src/hooks/useSharedState";
-// import { useBroadcasts } from "src/hooks/useSWR/useBroadcasts";
+import { useBroadcasts } from "src/hooks/useSWR/useBroadcasts";
 import { useSubscribeBroadcasts } from "src/hooks/useSubscribe";
 
 const Broadcasts: NextPage = () => {
   const { broadcastId, setBroadcastId } = useBroadcastId();
-  const broadcasts = useSubscribeBroadcasts();
+  // const broadcasts = useSubscribeBroadcasts();
 
-  // const { broadcasts, isLoading, error } = useBroadcasts();
+  const { broadcasts, isLoading, error } = useBroadcasts();
 
   useEffect(() => {
     setBroadcastId("");
   }, []);
 
-  // if (isLoading) return <div>Loading...</div>;
-  // if (error) return <div>{error.message}</div>;
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
 
   return (
     <BaseLayout title="放送一覧">
