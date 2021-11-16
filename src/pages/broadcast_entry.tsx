7ede52dd29a2a52ce28f6113ba56b7cd2e330285
boo <standingup.boo@gmail.com>
2021-11-16 20:24:36 +0900

    🎨:エンジビア投稿画面作成

--- src/pages/broadcast_entry.tsx
+++ src/pages/broadcast_entry.tsx
@@ -1,13 +1,45 @@
 import { BaseLayout } from "src/components/Layouts/BaseLayout";
 import { Label } from "src/components/Label";
+import { ChangeEvent, useState } from "react";
+import { TextArea } from "src/components/Form/TextArea";
 
 const BroadcastEntry = () => {
   // Todo: BEFORE|IN_PROGRESS|DONEいずれかが入る Hooks導入次第変更
   const status = "BEFORE";
 
+  // Todo: React Hooks Form を導入次第変更
+  const [entry, setEntry] = useState({
+    title: "",
+  });
+
+  const onChangeEntryHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    console.log(e.target.value);
+  };
+
   return (
-    <BaseLayout title="エンジビアを入力">
-      <Label status={status} />
+    <BaseLayout title="放送を作成">
+      <div className="mx-auto max-w-3xl">
+        <Label status={status} className="pt-16 text-center" />
+        <h1 className="py-10 mx-auto text-4xl font-bold text-center text-gray-900">
+          第4回エンジビアの泉
+        </h1>
+        <form>
+          {/* Todo: validationのパターンを検討 */}
+          <div className="flex flex-col gap-10 w-full">
+            <TextArea
+              value={entry.title}
+              placeholder="エンジビアを入力する"
+              rows={3}
+              onChange={onChangeEntryHandler}
+            />
+            <div className="space-x-4 w-full text-center">
+              <button className="py-3 px-8 text-white bg-light-blue-600 rounded-md">
+                保存する
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </BaseLayout>
   );
 };
