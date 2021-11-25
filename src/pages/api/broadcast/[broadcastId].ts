56b77309c1b169524032bbd330104206a590ce75
mu0363 <dde036@gmail.com>
2021-11-25 17:32:35 +0900

    不要なapiを削除

--- src/pages/api/broadcast/[broadcastId].ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { getBroadcast } from "src/lib/db-admin";
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const broadcast = await getBroadcast(req.query.broadcastId as string);
-  res.status(200).json({ broadcast });
-};
