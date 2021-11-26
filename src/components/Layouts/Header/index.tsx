573d8c6541587e5a1cf520fa7c3563f55c329008
mu0363 <dde036@gmail.com>
2021-11-26 10:18:34 +0900

    ヘッダーメニューを削除

--- src/components/Layouts/Header/index.tsx
+++ src/components/Layouts/Header/index.tsx
@@ -1,9 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Menu, Transition } from "@headlessui/react";
-import { Fragment } from "react";
 import { UserIcon } from "@heroicons/react/solid";
-import { LogoutIcon, IdentificationIcon } from "@heroicons/react/outline";
 import { signOut, useSession } from "next-auth/client";
 import { useUser } from "src/hooks/useSharedState";
 import { users } from "src/constant/users";
@@ -59,83 +56,19 @@ export const Header = () => {
           userC
         </button>
 
+        <button
+          onClick={() => signOut({ callbackUrl: "/" })}
+          className="py-2 px-4 hover:bg-gray-100 rounded-md border-2"
+        >
+          ログアウト
+        </button>
         {session?.user ? (
-          <div>
-            <Menu as="div">
-              <Menu.Button>
-                <img
-                  className="h-10 rounded-full"
-                  src={session && session.user.image}
-                  alt="avatar"
-                />
-              </Menu.Button>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-              >
-                <Menu.Items className="absolute right-0 mt-2 w-56 bg-white rounded-md divide-y divide-gray-100 ring-1 ring-black ring-opacity-5 shadow-lg origin-top-right focus:outline-none">
-                  <div className="py-1 px-1">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          className={`${
-                            active
-                              ? "bg-light-blue-600 text-white"
-                              : "text-gray-600"
-                          } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
-                        >
-                          {active ? (
-                            <IdentificationIcon
-                              className="mr-2 w-5 h-5"
-                              aria-hidden="true"
-                            />
-                          ) : (
-                            <IdentificationIcon
-                              className="mr-2 w-5 h-5"
-                              aria-hidden="true"
-                            />
-                          )}
-                          アカウント編集
-                        </button>
-                      )}
-                    </Menu.Item>
-                  </div>
-                  <div className="py-1 px-1">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <button
-                          onClick={() => signOut({ callbackUrl: "/" })}
-                          className={`${
-                            active
-                              ? "bg-light-blue-600 text-white"
-                              : "text-gray-600"
-                          } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
-                        >
-                          {active ? (
-                            <LogoutIcon
-                              className="mr-2 w-5 h-5"
-                              aria-hidden="true"
-                            />
-                          ) : (
-                            <LogoutIcon
-                              className="mr-2 w-5 h-5"
-                              aria-hidden="true"
-                            />
-                          )}
-                          ログアウト
-                        </button>
-                      )}
-                    </Menu.Item>
-                  </div>
-                </Menu.Items>
-              </Transition>
-            </Menu>
-          </div>
+          /* Todo: next.jsのImageが使えないので一旦このまま */
+          <img
+            className="h-10 rounded-full"
+            src={session && session.user.image}
+            alt="avatar"
+          />
         ) : (
           /* Todo: next.jsのImageが使えないので一旦このまま */
           <UserIcon className="p-1 h-10 text-gray-500 bg-gray-200 rounded-full" />
