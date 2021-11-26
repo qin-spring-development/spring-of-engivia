3c5ec2ee4afebcaff2f571cd2a909179bc43ff5f
boo <standingup.boo@gmail.com>
2021-11-27 00:24:17 +0900

    クリック時の動き微調整

--- src/components/SwitchButton/index.tsx
+++ src/components/SwitchButton/index.tsx
@@ -2,7 +2,7 @@ import useSound from "use-sound";
 import { useState } from "react";
 
 export type Props = {
-  playbackRate: string;
+  playbackRate: number;
 };
 
 export const SwitchButton = () => {
@@ -20,14 +20,14 @@ export const SwitchButton = () => {
           style={{ borderRadius: "100px / 40px", boxShadow: "0 8px 0 #c4cacc" }}
         ></span>
         <span
-          className="absolute before:absolute top-0 before:-top-7 hover:top-1 active:top-5 before:left-0 left-5 mt-8 w-40 before:w-40 before:h-14 h-14 hover:h-14 active:h-10 bg-light-blue-700 before:bg-light-blue-600 before:rounded-[80px/30px]"
+          className="absolute before:absolute top-0 before:-top-7 hover:top-1 active:top-4 before:left-0 left-5 mt-8 w-40 before:w-40 before:h-14 h-14 hover:h-[52px] active:h-10 bg-light-blue-700 before:bg-light-blue-600 before:rounded-[80px/30px]"
           style={{
             transition: "all 0.3s",
             borderRadius: "0 0 50% 50%",
           }}
         >
           <button onClick={() => play()}>
-            <span className="absolute -top-6 left-0 w-full text-4xl font-bold text-center text-gray-200">
+            <span className="absolute -top-7 left-0 py-2 w-full h-20 text-4xl font-bold text-center text-gray-200">
               へぇ
             </span>
           </button>
