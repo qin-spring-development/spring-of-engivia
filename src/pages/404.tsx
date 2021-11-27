ff6f375d5de3d5e2a432a1b0b7eb511629a7a340
compact_pro git config --global user.name compact_pro <gppmc689@yahoo.co.jp>
2021-11-28 00:27:34 +0900

    page404変更

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,25 @@
 import { BaseLayout } from "src/components/Layouts/BaseLayout";
+import Link from "next/link";
 
 export default function Custom404() {
   return (
     <BaseLayout title="404:ページが見つかりません">
       <div className="flex justify-center ">
-        <h1 className="flex items-center h-screen text-3xl font-bold">
-          ページが見つかりません
-        </h1>
-        {/* <link rel="" href="">Topに戻る</link> */}
+        <div className="flex items-center h-screen text-3xl font-bold">
+          <ul>
+            <li className="text-gray-900">
+              お探しのページは見つかりませんでした。
+            </li>
+            <li className="text-gray-900">
+              URLをご確認の上、再度お試しください。
+            </li>
+            <Link href="/">
+              <button className="text-xl text-light-blue-600">
+                トップへ戻る
+              </button>
+            </Link>
+          </ul>
+        </div>
       </div>
     </BaseLayout>
   );
