14b43f907983e3c007c02963dfe2386222a20a06
boo <standingup.boo@gmail.com>
2021-12-05 23:15:20 +0900

    ♻️

--- src/components/SwitchButton/index.tsx
+++ src/components/SwitchButton/index.tsx
@@ -7,7 +7,6 @@ import { useSession } from "next-auth/client";
 import { User } from "next-auth";
 
 export type Props = {
-  classNameLock?: string;
   broadcastId: string;
   featureEngivia: EngiviaType;
   likes: number;
@@ -17,7 +16,6 @@ export const SwitchButton: FC<Props> = ({
   broadcastId,
   featureEngivia,
   likes,
-  classNameLock,
 }) => {
   const [session] = useSession();
   const [playbackRate, setPlaybackRate] = useState(0);
@@ -32,29 +30,26 @@ export const SwitchButton: FC<Props> = ({
     await updateTotalLikes(broadcastId, featureEngivia.id);
   };
 
-  switch (true) {
-    case likes <= 20:
-      classNameLock = "hover:h-[52px] active:h-10  hover:top-1 active:top-4 ";
-      break;
-    default:
-      break;
-  }
-
   return (
     <div>
       <div className="block relative m-0 w-[200px] h-32">
         <span
           className="absolute top-9 left-0 w-[200px] h-20 bg-gray-100"
-          style={{ borderRadius: "100px / 40px", boxShadow: "0 8px 0 #c4cacc" }}
+          style={{
+            borderRadius: "100px / 40px",
+            boxShadow: "0 8px 0 #c4cacc",
+          }}
         ></span>
         <span
-          className={`absolute before:absolute top-0 before:-top-7 before:left-0 left-5 mt-8 w-40 before:w-40 before:h-14 h-14 bg-light-blue-700 before:bg-light-blue-600 before:rounded-[80px/30px] ${classNameLock}`}
+          className={`absolute before:absolute top-0 before:-top-7 before:left-0 left-5 mt-8 w-40 before:w-40 before:h-14 h-14 bg-light-blue-700 before:bg-light-blue-600 before:rounded-[80px/30px] ${
+            likes < 20 && "hover:h-[52px] active:h-10  hover:top-1 active:top-4"
+          }`}
           style={{
             transition: "all 0.3s",
             borderRadius: "0 0 50% 50%",
           }}
         >
-          <button disabled={likes >= 20 && true} onClick={handleClick}>
+          <button disabled={likes >= 20} onClick={handleClick}>
             <span className="absolute -top-7 left-0 py-2 w-full h-20 text-4xl font-bold text-center text-gray-200">
               へぇ
             </span>
