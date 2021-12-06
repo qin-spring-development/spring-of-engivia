5c27d45acaa7e3000772697991eb03c4ec4e6a8b
mu0363 <dde036@gmail.com>
2021-12-06 13:40:06 +0900

    計算式を修正

--- src/lib/sumTotalLikes.ts
+++ src/lib/sumTotalLikes.ts
@@ -1,5 +1,5 @@
 export const sumTotalLikes = (joinUsersLength: number, totalLikes: number) => {
   const sumTotalLikes =
-    (Math.round((totalLikes / joinUsersLength) * 5) * 10) / 10;
+    Math.round((totalLikes / joinUsersLength) * 5 * 10) / 10;
   return sumTotalLikes;
 };
