eaa4ea8dd467d6cfe72e56bc2d0018f1ea62fd14
mu0363 <dde036@gmail.com>
2021-12-07 21:32:35 +0900

    import typeへ変更

--- src/components/Engivia/EngiviaCardWithTotalLikes/index.tsx
+++ src/components/Engivia/EngiviaCardWithTotalLikes/index.tsx
@@ -1,4 +1,5 @@
-import { FC, useEffect, useState } from "react";
+import type { FC } from "react";
+import { useEffect, useState } from "react";
 import { EngiviaType } from "src/types/interface";
 
 type Props = {
