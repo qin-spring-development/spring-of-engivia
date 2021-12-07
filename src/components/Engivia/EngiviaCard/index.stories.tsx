2eb87549bb540b2cf8123de74135397ba86ea68b
boo <standingup.boo@gmail.com>
2021-12-07 23:27:21 +0900

    EngiviaCard storybookに登録

---
+++ src/components/Engivia/EngiviaCard/index.stories.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react/types-6-0";
+import { EngiviaCard, Props } from "./index";
+
+export default {
+  component: EngiviaCard,
+  title: "Components/EngiviaCard",
+} as Meta;
+
+const Template: Story<Props> = (args) => <EngiviaCard {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  engivia: {
+    id: "1",
+    engiviaNumber: 1,
+    body: "ここにエンジビアが入ります。",
+    createdAt: "おさむ",
+    featureStatus: "BEFORE",
+    postUser: { image: "./15007672.jpeg", name: "おさむ", id: "000000000" },
+    totalLikes: 20,
+  },
+};
