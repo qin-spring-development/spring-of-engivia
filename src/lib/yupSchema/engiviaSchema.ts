c237af0d6f6ea25d2431aa621ddf534389cbd6dc
shogo4131 <shougo80697118@gmail.com>
2021-12-12 01:04:03 +0900

    fix: add validation schemas

--- src/lib/yupSchema/engiviaSchema.ts
+++ src/lib/yupSchema/engiviaSchema.ts
@@ -3,9 +3,14 @@ import * as yup from "yup";
 const REQUIRE_MSG = "エンジビアを入力してください";
 const VIOLATION_PASSWORD_MAX_COUNT = "100文字以内で入力してください";
 
-export const engiviaSchema = yup.object().shape({
-  engivia: yup
-    .string()
-    .required(REQUIRE_MSG)
-    .max(100, VIOLATION_PASSWORD_MAX_COUNT),
-});
+const schemas = () => {
+  return yup.object().shape({
+    engivia: yup
+      .string()
+      .required(REQUIRE_MSG)
+      .max(100, VIOLATION_PASSWORD_MAX_COUNT),
+    username: yup.string().required("必須").max(21),
+  });
+};
+
+export default schemas;
