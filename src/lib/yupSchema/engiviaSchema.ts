5a53252638caa7d450a5b5034ff3a37f98472457
shogo4131 <shougo80697118@gmail.com>
2021-12-16 23:03:08 +0900

    refactor/add error message file

--- src/lib/yupSchema/engiviaSchema.ts
+++ src/lib/yupSchema/engiviaSchema.ts
@@ -1,14 +1,14 @@
 import * as yup from "yup";
-
-// Todo: 入力内容は別ファイルに分けた方が良い
-const REQUIRE_MSG = "エンジビアを入力してください";
-const ACCOUNT_NAME_REQUIRE = "アカウント名を入力してください";
-const TITLE_REQUIRE = "タイトルを入力してください";
-const DATE_REQUIRE = "日付を入力してください";
-const TITLE_REQUIRE_MAX_COUNT = "タイトルは100文字以内で入力してください";
-const DATE_MAX_COUNT = "日付はYYYY/MM/DD形式で入力してください";
-const ACCOUNT_NAME_MAX_COUNT = "アカウント名は21文字以内で入力してください";
-const VIOLATION_PASSWORD_MAX_COUNT = "100文字以内で入力してください";
+import {
+  REQUIRE_MSG,
+  VIOLATION_PASSWORD_MAX_COUNT,
+  ACCOUNT_NAME_MAX_COUNT,
+  ACCOUNT_NAME_REQUIRE,
+  TITLE_REQUIRE,
+  TITLE_REQUIRE_MAX_COUNT,
+  DATE_MAX_COUNT,
+  DATE_REQUIRE,
+} from "src/constant/errorMessage";
 
 const schemas = () => {
   return yup.object().shape({
@@ -24,8 +24,6 @@ const schemas = () => {
       .string()
       .required(TITLE_REQUIRE)
       .max(100, TITLE_REQUIRE_MAX_COUNT),
-    // Todo: 日付のバリデーションで前の日を選択できないようにする
-    // Todo: 日付の形式と入力文字でバリデーションを分ける
     broadCastingDate: yup
       .string()
       .required(DATE_REQUIRE)
