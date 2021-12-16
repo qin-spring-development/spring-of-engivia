3353725d0afacc2dbb71428a78383ac98045aa1c
shogo4131 <shougo80697118@gmail.com>
2021-12-16 23:01:48 +0900

    refactor/change form

--- src/components/Form/InputFiled/index.tsx
+++ src/components/Form/InputFiled/index.tsx
@@ -1,34 +1,25 @@
-import { FC } from "react";
+import type { FC, ComponentProps } from "react";
 import { UseFormRegisterReturn } from "react-hook-form";
 
 export type Props = {
   className?: string;
-  type: string;
   value?: string;
-  id?: string;
-  min?: string;
-  placeholder?: string;
   register: UseFormRegisterReturn;
-};
+} & ComponentProps<"input">;
 
 export const InputFiled: FC<Props> = ({
   className,
-  type = "text",
   value,
-  id,
-  min,
-  placeholder,
   register,
+  ...rest
 }) => {
   return (
     <div className={className}>
       <input
-        type={type}
-        id={id}
+        type="text"
         defaultValue={value}
-        min={min}
         className="py-1 px-3 w-full text-base leading-8 text-gray-500 bg-white rounded-md border border-gray-300 focus:ring outline-none"
-        placeholder={placeholder}
+        {...rest}
         {...register}
       />
     </div>
--- src/components/Form/TextArea/index.tsx
+++ src/components/Form/TextArea/index.tsx
@@ -1,37 +1,24 @@
-import { FC } from "react";
+import type { FC, ComponentProps } from "react";
 import { UseFormRegisterReturn } from "react-hook-form";
 
 export type Props = {
   className?: string;
   value?: string;
-  id?: string;
-  placeholder?: string;
-  rows: number;
-  maxlength: number;
-  cols?: number;
   register: UseFormRegisterReturn;
-};
+} & ComponentProps<"textarea">;
 
 export const TextArea: FC<Props> = ({
   className,
   value,
-  id,
-  placeholder,
-  rows,
-  cols,
-  maxlength,
   register,
+  ...rest
 }) => {
   return (
     <div>
       <textarea
-        id={id}
         defaultValue={value}
         className={`py-2 px-3 w-full text-base leading-8 text-gray-500 border border-gray-300 bg-white rounded-md focus:ring outline-none ${className}`}
-        placeholder={placeholder}
-        rows={rows}
-        cols={cols}
-        maxLength={maxlength}
+        {...rest}
         {...register}
       />
     </div>
